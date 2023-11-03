218a1bb86de3bc3fc9880360903fdce193af17e3
Melissa LeBlanc-Williams <melissa@adafruit.com>
2023-11-03 13:39:05 -0700

    Add more mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -26,8 +26,15 @@ extensions = [
 # Uncomment the below if you use native CircuitPython modules such as
 # digitalio, micropython and busio. List the modules you use. Without it, the
 # autodoc module docs will fail to generate with a warning.
-autodoc_mock_imports = ["dotclockframebuffer", "framebufferio"]
-
+autodoc_mock_imports = [
+    "supervisor",
+    "rtc",
+    "dotclockframebuffer",
+    "framebufferio" "ssl",
+    "wifi",
+    "socketpool",
+    "bitmaptools",
+]
 autodoc_preserve_defaults = True
 
 
