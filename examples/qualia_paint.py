66c33bb95c8c83da1b8c3b2e409e05c619ab9e5c
Melissa LeBlanc-Williams <melissa@adafruit.com>
2023-11-06 12:10:04 -0800

    Fix Display ID in paint example

--- examples/qualia_paint.py
+++ examples/qualia_paint.py
@@ -9,7 +9,7 @@ from adafruit_qualia.graphics import Graphics, Displays
 # For other displays:
 # 2.1" Round = Displays.ROUND21
 # 3.4" Square = Displays.SQUARE34
-# 320 x 820 Bar - Displays.BAR320x820
+# 320 x 820 Bar - Displays.BAR320X820
 graphics = Graphics(Displays.SQUARE40, default_bg=None, auto_refresh=False)
 
 if graphics.touch is None:
